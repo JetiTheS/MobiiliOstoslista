2120cda3ba6446a645bc7683ec0cea88dba8f14f
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-09-01 10:48:35 +0300

    Ostoslista tehty

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [shopping, setShopping] = useState("");
+  const [shoppings, setShoppings] = useState([]);
+  console.log(shoppings);
+
+  const handlePress = () => {
+    setShoppings([...shoppings, { key: shopping }]);
+    setShopping("");
+  }
+  const handleClear = () => {
+    setShoppings([]);
+    setShopping("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        placeholder='Enter a new item...'
+        onChangeText={text => setShopping(text)}
+        value={shopping}
+      />
+      <View style={styles.button}>
+        <Button
+          title="Add" onPress={handlePress}
+        />
+        <Button
+          title='Clear' onPress={handleClear}
+        />
+      </View>
+      <Text style={styles.shopping}>Shoppinglist</Text>
+      <FlatList
+        data={shoppings}
+        renderItem={({ item }) => <Text style={styles.itemText}>{item.key}</Text>}
+        ListEmptyComponent={<Text>No grocerys</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +48,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50
   },
+  shopping: {
+    marginTop: 40, color: "blue", fontSize: 20, fontWeight: "bold"
+  },
+  itemText: {
+    width: 100, textAlign: "center",
+  },
+  button: {
+    flexDirection: "row",
+  }
 });
